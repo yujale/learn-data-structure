62485bf5dfc213d9ae214f975113e51dc49a379d
yujale98 <yujale98@gmail.com>
2024-03-19 19:46:59 +0800

    feat:leetcode 268 丢失的数字

---
+++ src/algorithm/leetCode/sort/_268_丢失的数字.java
@@ -0,0 +1,24 @@
+package algorithm.leetCode.sort;
+
+public class _268_丢失的数字 {
+
+
+    public int missingNumber(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
